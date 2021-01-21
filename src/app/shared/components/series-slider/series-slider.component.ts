de55778f994e3ca66ee716286d8548ef2ec86734
AgustinMorinigo <agustingr3en@hotmail.com>
2021-01-21 00:27:57 -0300

    styling the series slider

--- src/app/shared/components/series-slider/series-slider.component.ts
+++ src/app/shared/components/series-slider/series-slider.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Serie } from '@shared/interfaces/series-response.interface';
 import { SeriesService } from '@shared/services/series.service';
+import Swiper, { Pagination, Autoplay } from 'swiper';
+Swiper.use([ Pagination, Autoplay ]);
 
 @Component({
   selector: 'app-series-slider',
   templateUrl: './series-slider.component.html',
   styleUrls: ['./series-slider.component.scss']
 })
-export class SeriesSliderComponent implements OnInit {
+export class SeriesSliderComponent implements OnInit, AfterViewInit {
   public title: string = "Series populares";
   public series: Serie[];
 
@@ -16,7 +18,19 @@ export class SeriesSliderComponent implements OnInit {
   ngOnInit(): void {
     this.seriesService.getPopularSeries().subscribe( popularSeries => {
       this.series = popularSeries.splice(0, 10);
+      console.log(this.series);
     });
+  }
+
+  ngAfterViewInit() {
+    const mySwiper = new Swiper('.series-swiper-container', {
+      pagination: {
+        el: '.series-swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+      },
+      slidesPerView: 2
+    })
   }
 
 }

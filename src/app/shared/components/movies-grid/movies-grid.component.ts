0c0480875f2cd46740edcddd090e92dafa2c298c
AgustinMorinigo <agustingr3en@hotmail.com>
2021-02-04 04:27:10 -0300

    working on the movie details component

--- src/app/shared/components/movies-grid/movies-grid.component.ts
+++ src/app/shared/components/movies-grid/movies-grid.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { Movie } from '@shared/interfaces/movies-response.interface';
 import { MoviesService } from '@shared/services/movies.service';
 
@@ -10,10 +11,15 @@ import { MoviesService } from '@shared/services/movies.service';
 export class MoviesGridComponent implements OnInit {
   @Input() movies: Movie[];
 
-  constructor( private moviesService: MoviesService ) { }
+  constructor( private moviesService: MoviesService,
+               private router: Router ) { }
 
   ngOnInit(): void {
     // console.log(this.movies);
   }
 
+  showDetails( id: number ) {
+    this.router.navigate(['película-detalles', id]);
+    window.scrollTo(0,0);
+  }
 }
